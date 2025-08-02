a012e5a276fefc8f734c7db26b90372bb37ec61d
Marco Mustapic <marco.mustapic@inthepocket.com>
2025-08-02 21:58:04 +0200

    Initial Commit

---
+++ Mastermind/MastermindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MastermindApp.swift
+//  Mastermind
+//
+//  Created by Marco Mustapic on 02/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MastermindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MastermindTests/MastermindTests.swift
@@ -0,0 +1,17 @@
+//
+//  MastermindTests.swift
+//  MastermindTests
+//
+//  Created by Marco Mustapic on 02/08/2025.
+//
+
+import Testing
+@testable import Mastermind
+
+struct MastermindTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
