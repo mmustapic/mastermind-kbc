75ce4a04d7bfee31d0ccdc484865941c51acbef9
Marco Mustapic <marco.mustapic@inthepocket.com>
2025-08-03 23:19:09 +0200

    ui tests

--- MastermindUITests/MastermindUITests.swift
+++ MastermindUITests/MastermindUITests.swift
@@ -10,34 +10,48 @@ import XCTest
 final class MastermindUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     @MainActor
-    func testExample() throws {
+    func testWin() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
+        app.launchArguments.append("--uitesting")
         app.launch()
+        app.textFields["inputView0"].tap()
+        app.textFields["inputView0"].typeText("A")
+        app.textFields["inputView1"].tap()
+        app.textFields["inputView1"].typeText("B")
+        app.textFields["inputView2"].tap()
+        app.textFields["inputView2"].typeText("C")
+        app.textFields["inputView3"].tap()
+        app.textFields["inputView3"].typeText("D")
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(app.staticTexts["answerText"].waitForExistence(timeout: 20.0))
+        XCTAssertEqual(app.staticTexts["answerText"].label, "Answer was ABCD")
+        XCTAssertEqual(app.staticTexts["statusText"].label, "You Won!")
+        XCTAssertEqual(app.buttons["playAgainButton"].label, "Play again")
     }
 
     @MainActor
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    func testLose() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launchArguments.append("--uitesting")
+        app.launch()
+        app.textFields["inputView0"].tap()
+        app.textFields["inputView0"].typeText("W")
+        app.textFields["inputView1"].tap()
+        app.textFields["inputView1"].typeText("X")
+        app.textFields["inputView2"].tap()
+        app.textFields["inputView2"].typeText("Y")
+        app.textFields["inputView3"].tap()
+        app.textFields["inputView3"].typeText("Z")
+
+        XCTAssertTrue(app.staticTexts["answerText"].waitForExistence(timeout: 20.0))
+        XCTAssertEqual(app.staticTexts["answerText"].label, "Answer was ABCD")
+        XCTAssertEqual(app.staticTexts["statusText"].label, "You Lost!")
+        XCTAssertEqual(app.buttons["playAgainButton"].label, "Play again")
     }
 }
